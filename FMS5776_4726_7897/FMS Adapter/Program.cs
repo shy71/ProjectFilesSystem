07adaf426a2bb9d70df818850666ff7831df0746
ee bb <ezra@anyblock.com>
2016-06-09 18:14:37 +0300

    sdfsdf

--- FMS5776_4726_7897/FMS Adapter/Program.cs
+++ FMS5776_4726_7897/FMS Adapter/Program.cs
@@ -516,14 +516,14 @@ namespace FMS_adapter
                 Console.WriteLine("Marshal.SizeOf(typeof(VolumeHeader) == " + structSize);
                 Disk d = new Disk();
                 Console.WriteLine("\nMake Disk:");
-                Console.WriteLine(ToStringProperty(d.GetVolumeHeader()));
+//                Console.WriteLine(ToStringProperty(d.GetVolumeHeader()));
                 d.Createdisk("disk1", "oshri");
                 Console.WriteLine("\nCreate Disk:");
-                Console.WriteLine(ToStringProperty(d.GetVolumeHeader()));
+               // Console.WriteLine(ToStringProperty(d.GetVolumeHeader()));
                 d.MountDisk("disk1");
                 d.Format("oshri");
                 Console.WriteLine("\nFormat Disk:");
-                Console.WriteLine(ToStringProperty(d.GetVolumeHeader()));
+               // Console.WriteLine(ToStringProperty(d.GetVolumeHeader()));
 
 
                 d.CreateFile("File1", "Ezra", "F", 20, 20, "I", 0);
