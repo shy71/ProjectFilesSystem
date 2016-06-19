f3dda4c5abae14360c96382b2c49ae3942d2198e
ee bb <ezra@anyblock.com>
2016-06-19 14:52:13 +0300

    sync

--- FMS5776_4726_7897/FMS GUI/Opening Record.xaml.cs
+++ FMS5776_4726_7897/FMS GUI/Opening Record.xaml.cs
@@ -42,6 +42,11 @@ namespace FMS_GUI
         }
         private void DelField_Click(object sender, RoutedEventArgs e)
         {
+            if (Fields.SelectedItem == null)
+            {
+                MessageBox.Show("You haven't seleted anything to edit.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
             UneditableField field = (UneditableField)Fields.SelectedItem;
             string f = field.FieldName.Text + "," + field.FieldContent.Text;
             fields.Remove(f);
